1d726a03e157df538906beda7b7c09a7b75c2f19
nicoxeye <nicoxeye@op.pl>
2024-10-27 22:39:43 +0100

    fix: import_from_file

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 #IMPORT
-def import_from_file(filename):
+def import_from_file(filepath):
     students = []
-    with open(filename, 'r') as file:
+    with open(filepath, 'r') as file: #'r' read file
         for line in file:
             cut_parts = line.strip().split(',') #delete whitespaces and create a seperator as ,
             #in the file: first name, last name
-            if len(cut_parts) == 2: # <- check if theres the first name and last name in the file
-                students.append
-                (
-                    {
+            if len(cut_parts) == 2 or len(cut_parts) == 3: # <- check if theres the first name and last name in the file OR WITH ATTENDANCE ALREADY WRITTEN
+                students.append({
                     'first_name': cut_parts[0].strip(),
                     'last_name': cut_parts[1].strip(),
                     'present': False #automatically adds false to if someone is present
-                    }
-                )
+                    })
             else: print('something went wrong when importing the file.')
+    return students
 # can probably add an except to return an error if it doesnt find the file but idrk how to do that:3
 
 #EXPORT
