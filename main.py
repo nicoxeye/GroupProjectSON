940cc19fd6b11d69c8ceae96ad0363a9be11eb82
wera <ciastkouni@gmail.com>
2024-10-28 20:14:57 +0100

    feat: option to add new students or update database

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import csv
+import os
+
 #IMPORT
 def import_from_file(filename):
     students = []
@@ -16,5 +19,45 @@ def import_from_file(filename):
                 )
             else: print('something went wrong when importing the file.')
 # can probably add an except to return an error if it doesnt find the file but idrk how to do that:3
+            
+#ADDING NEW STUDENTS AND UPDATING DATABASE
 
-#EXPORT
+# function that adds new student to the list and saves it to the file
+def add_student(first_name, last_name, filename="students.csv"):
+    file_exists = os.path.isfile(filename)
+    
+    with open(filename, 'a', newline='') as file:
+        writer = csv.writer(file)
+        
+        if not file_exists:
+            writer.writerow(["first_name", "last_name", "present"])
+        
+        # "present" is false by default
+        writer.writerow([first_name, last_name, False])
+    print(f"Student {first_name} {last_name} was added to {filename}.")
+
+# function that edits student list
+def edit_student(old_first_name, old_last_name, new_first_name, new_last_name, filename="students.csv"):
+    students = []
+    updated = False
+
+    with open(filename, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            if row["first_name"] == old_first_name and row["last_name"] == old_last_name:
+                row["first_name"] = new_first_name
+                row["last_name"] = new_last_name
+                updated = True
+            students.append(row)
+    
+    with open(filename, 'w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=["first_name", "last_name", "present"])
+        writer.writeheader()
+        writer.writerows(students)
+    
+    if updated:
+        print(f"Student: {old_first_name} {old_last_name} has been updated to {new_first_name} {new_last_name}.")
+    else:
+        print("The student hasn't been found")
+
+            
